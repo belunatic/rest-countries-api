91424ceaf78f491afcc37b21cb19b9d202626646
belunatic <abelluvyou@yahoo.com>
2025-10-22 11:07:34 -0700

    theme setting and storing in localStorage

--- src/js/utility.ts
+++ src/js/utility.ts
@@ -1,6 +1,37 @@
 //dark mode switch
 const icon = document.getElementById("icon") as HTMLSpanElement;
 
+let currentTheme: string = "dark";
+
+document.addEventListener("DOMContentLoaded", () => {
+	//stored theme
+	const storedTheme = localStorage.getItem("theme");
+	if (storedTheme !== null) {
+		currentTheme = storedTheme;
+	} else {
+		console.log("Theme is not set yet");
+	}
+
+	//add the theme class
+	// get all element with dark class
+	const originalDarkClassElement = document.querySelectorAll(".mode");
+
+	//icon update and theme
+	if (currentTheme === "dark") {
+		icon.setAttribute("name", "moon");
+		originalDarkClassElement.forEach((item) => {
+			console.log(item.classList, " This is the query ", item);
+			item.classList.add("dark");
+		});
+	} else {
+		icon.setAttribute("name", "moon-outline");
+		originalDarkClassElement.forEach((item) => {
+			console.log(item.classList, " This is the query ", item);
+			item.classList.remove("dark");
+		});
+	}
+});
+
 icon.addEventListener("click", (e: Event) => {
 	const target = e.target as HTMLElement;
 	//change the Icon
@@ -17,4 +48,11 @@ icon.addEventListener("click", (e: Event) => {
 		console.log(item.classList, " This is the query ", item);
 		item.classList.toggle("dark");
 	});
+
+	//set the theme in localStorage
+	if (currentTheme === "dark") {
+		localStorage.setItem("theme", "light");
+	} else {
+		localStorage.setItem("theme", "dark");
+	}
 });
